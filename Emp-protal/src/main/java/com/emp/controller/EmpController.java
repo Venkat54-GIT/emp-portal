c980fa4c49d72f90e30e13fa966dd3cc01ea1b28
Venkat <venkatasainagagurgarao@gmail.com>
2025-08-27 08:27:04 +0530

    intial commit for emp-portal

---
+++ Emp-protal/src/main/java/com/emp/controller/EmpController.java
@@ -0,0 +1,13 @@
+package com.emp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmpController {
+
+	@GetMapping(value="/")
+	public String getWelcomeMessage() {
+		return "Welcome to Emp Portal";
+	}
+}
